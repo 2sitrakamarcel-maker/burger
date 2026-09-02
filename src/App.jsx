30e968d6024f20be6f627d24642bc3b79e3939cd
shytoosy <2sitrakamarcel@gmail.com>
2026-09-02 17:38:08 +0300

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>VA TE FAIRE FOUTRE</h1>
+          <h1>VA TE FAIRE FOUTRE gr</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
